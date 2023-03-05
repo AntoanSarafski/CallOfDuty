906293412ebe5be27556fe6b46a42ee3fa4950fd
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-05 13:43:19 +0200

    Add project files.

---
+++ CallOfDuty/Models/Interfaces/IPrivate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallOfDuty.Models.Interfaces
+{
+    public interface IPrivate : ISoldier
+    {
+
+    }
+}
---
+++ CallOfDuty/Models/Interfaces/ISoldier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallOfDuty.Models.Interfaces
+{
+    public interface ISoldier
+    {
+        public int Id { get; }
+
+        public string FirstName { get; }
+
+        public int LastName { get; }
+
+    }
+}
